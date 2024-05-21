3c665e03ac72637264c3ee514d9f899115f9a82c
Moa Ha <moahaa03@gmail.com>
2024-05-21 21:55:13 +1200

    route the layout

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,3 +1,8 @@
 import { createRoutesFromElements, Route } from 'react-router-dom'
-import App from './components/App.tsx'
-export default createRoutesFromElements(<Route index element={<App />} />)
+import Home from './pages/Home.tsx'
+import Layout from './components/Layout.tsx'
+export default createRoutesFromElements(
+  <Route path="/" element={<Layout />}>
+    <Route index element={<Home />} />
+  </Route>,
+)
