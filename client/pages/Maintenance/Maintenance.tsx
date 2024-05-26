32b68caad9190d213c4e6c60bdd3d9f56f2130b6
Moa Ha <moahaa03@gmail.com>
2024-05-26 17:50:05 +1200

    make page

--- client/pages/Maintenance/Maintenance.tsx
+++ client/pages/Maintenance/Maintenance.tsx
@@ -1,5 +1,14 @@
 function Maintenance() {
-  return <>Maintenance</>
+  return (
+    <div className="flex flex-col gap-4 p-4">
+      <div className="bg-opacity p-4">
+        <div className="relative text-2xl">WoF</div>
+      </div>
+      <div className="bg-opacity p-4">
+        <div className="relative text-2xl ">Rego</div>
+      </div>
+    </div>
+  )
 }
 
 export default Maintenance
