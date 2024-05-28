52db1837533de0283bd80cdf4784754e88941f60
Moa Ha <moahaa03@gmail.com>
2024-05-28 22:43:07 +1200

    put some links

--- client/components/Maintenance/Rego.tsx
+++ client/components/Maintenance/Rego.tsx
@@ -1,7 +1,29 @@
+import { Link } from 'react-router-dom'
+
 function Rego() {
   return (
     <>
-      <div className="relative text-2xl">Rego</div>
+      <div className="relative text-2xl">
+        Rego
+        <p className="text-base">
+          <Link
+            to="https://transact.nzta.govt.nz/transactions/renewvehiclelicence/entry"
+            className="underline"
+          >
+            Renewal of vehicle licence (rego)
+          </Link>
+          : Renew your registration.
+        </p>
+        <p className="text-base">
+          <Link
+            to="https://transact.nzta.govt.nz/transactions/CheckExpiry/entry"
+            className="underline"
+          >
+            Check expiry query
+          </Link>
+          : Check when your registration due
+        </p>
+      </div>
     </>
   )
 }
