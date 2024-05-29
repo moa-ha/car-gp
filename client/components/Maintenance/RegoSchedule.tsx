2798c907b667172013bad9c1fb8c575a25e8470c
Moa Ha <moahaa03@gmail.com>
2024-05-29 14:15:02 +1200

    move function to sched comp

--- client/components/Maintenance/RegoSchedule.tsx
+++ client/components/Maintenance/RegoSchedule.tsx
@@ -1,11 +1,55 @@
-function RegoSchedule({ date }: { date: string }) {
+import { useState } from 'react'
+import { useMaintenance } from '../../hooks/useMaintenance'
+// TODO: separate wof vs rego
+
+function RegoSchedule() {
+  const { data } = useMaintenance()
+
+  const [formState, setFormState] = useState({
+    wof: data?.wof,
+    wofDue: data?.wofDue,
+    rego: data?.rego,
+    regoDue: data?.regoDue,
+  })
+  const [date, setDate] = useState('')
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    console.log(formState)
+
+    const { value } = e.currentTarget
+    setFormState((prev) => ({ ...prev, rego: value }))
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    console.log('passing ' + formState.rego)
+
+    setDate(formState.rego)
+  }
+
+  let due
+
   const handleClick = (month: number) => {
-    date = month * 31 + date
+    due = month * 31 + date
   }
 
   const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
   return (
     <>
+      <div className="p-4 text-base">
+        When was it replaced?
+        <form onSubmit={handleSubmit}>
+          <input
+            className="m-2 rounded border border-gray-300 px-4 py-2"
+            onChange={handleChange}
+            type="date"
+            value={formState.rego}
+            name="rego"
+          />
+          <button type="submit"> Save</button>
+          <RegoSchedule date={date} />
+        </form>
+      </div>
       <p className="text-lg">How many months have you registered?</p>
       {months.map((month) => (
         <button
@@ -17,8 +61,8 @@ function RegoSchedule({ date }: { date: string }) {
         </button>
       ))}
 
-      <p className="text-base">Renew before {date}</p>
-      {/* TODO: make push alrarm */}
+      <p className="text-base">Renew before {due}</p>
+      {/* TODO: make push alarm */}
       {/* <p className="text-base">Send notice ~~ before</p> */}
     </>
   )
