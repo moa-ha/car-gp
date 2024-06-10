4bc968c2bf552a18811644fe1b929913201de435
Moa Ha <moahaa03@gmail.com>
2024-06-11 00:26:29 +1200

    remove default items

--- server/db/consumables.ts
+++ server/db/consumables.ts
@@ -2,26 +2,8 @@ import db from './connection.ts'
 import { Consumable, ConsumableData } from '../../models/consumable.ts'
 
 export async function getConsumablesByUser(id: string): Promise<Consumable[]> {
-  return db('consumables').where('user', id).orWhere('user', 'default')
+  return db('consumables').where('user', id)
 }
-
-//adding not working with this
-// export async function addConsumable(
-//   consumable: ConsumableData,
-//   user: string,
-// ): Promise<Consumable> {
-//   const consumableAuthorized: ConsumableUser = {
-//     name: consumable.name,
-//     replaced: consumable.replaced,
-//     due: consumable.due,
-//     km: consumable.km,
-//     user: user,
-//   }
-
-//   return db('consumable').insert(consumableAuthorized)
-//   // .returning(columns)
-//   // .then((insertedEntries) => insertedEntries[0])
-// }
 
 export async function getConsumableById(id: number) {
   return await db('consumables').where({ id }).first()
