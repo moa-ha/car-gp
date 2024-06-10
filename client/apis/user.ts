28bdfbba367806dd86095460e1bccc5c4b4be35d
Moa Ha <moahaa03@gmail.com>
2024-06-11 00:09:28 +1200

    add default data for new user - api

--- client/apis/user.ts
+++ client/apis/user.ts
@@ -1,13 +1,51 @@
 import request from 'superagent'
 import { User } from '../../models/user'
 
-const rootUrl = '/api/v1'
+const rootUrl = '/api/v1/user'
 
 export async function getUsers(): Promise<User[]> {
   const res = await request.get(rootUrl)
   return res.body as User[]
 }
 
+// export async function addUser(user: User) {
+//   await request.post(rootUrl).send(user)
+// }
+
 export async function addUser(user: User) {
+  const defaultData = [
+    {
+      name: 'Engine Oil',
+      km: 10000,
+      user: user.id,
+    },
+    {
+      name: 'Brake Oil',
+      km: 40000,
+      user: user.id,
+    },
+    {
+      name: 'Mission Oil',
+      km: 80000,
+      user: user.id,
+    },
+    {
+      name: 'Distilled Water',
+      km: 20000,
+      user: user.id,
+    },
+    {
+      name: 'Battery',
+      km: 50000,
+      user: user.id,
+    },
+    {
+      name: 'Tyre',
+      km: 40000,
+      user: user.id,
+    },
+  ]
+
   await request.post(rootUrl).send(user)
+  await request.post('/api/v1/consumables').send(defaultData)
 }
