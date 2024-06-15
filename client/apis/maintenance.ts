86e4f0e8742607af28e0e2418cb2852c0163d152
Moa Ha <moahaa03@gmail.com>
2024-06-15 19:10:32 +1200

    FE wof update

--- client/apis/maintenance.ts
+++ client/apis/maintenance.ts
@@ -1,9 +1,21 @@
 import request from 'superagent'
-import { MaintenanceUser } from '../../models/maintenance'
+import { MaintenanceUser, Wof } from '../../models/maintenance'
 
 const rootUrl = '/api/v1/maintenance'
 
 export async function getMaintenance() {
   const res = await request.get(rootUrl)
   return res.body as MaintenanceUser
+}
+
+interface UpdateWof {
+  wof: Wof
+  token: string
+}
+export async function updateWof({ wof, token }: UpdateWof): Promise<void> {
+  return await request
+    .patch(rootUrl)
+    .send(wof)
+    .set('Authorization', `Bearer ${token}`)
+    .then((res) => res.body)
 }
--- client/hooks/useMaintenance.ts
+++ client/hooks/useMaintenance.ts
@@ -1,5 +1,8 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import * as api from '../apis/maintenance'
+import { useAuth0 } from '@auth0/auth0-react'
+import { Consumable } from '../../models/consumable'
+import { Wof } from '../../models/maintenance'
 
 export function useMaintenance() {
   const query = useQuery({
@@ -9,4 +12,17 @@ export function useMaintenance() {
   return {
     ...query,
   }
+}
+
+export function useUpdateWof() {
+  const { getAccessTokenSilently } = useAuth0()
+  const client = useQueryClient()
+
+  return useMutation({
+    mutationFn: async (wof: Wof) => {
+      const token = await getAccessTokenSilently()
+      return api.updateWof({ wof, token })
+    },
+    onSuccess: () => client.invalidateQueries({ queryKey: ['maintenance'] }),
+  })
 }
