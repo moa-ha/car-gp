b972eda00817e7da06a25fe97f232bcfdb8f0a26
Moa Ha <moahaa03@gmail.com>
2024-06-19 23:53:40 +1200

    make string default for client side data

--- client/apis/user.ts
+++ client/apis/user.ts
@@ -23,37 +23,53 @@ export async function newUser(user: User) {
   const defaultMaintenance = [
     {
       user: user.id,
+      wof: '',
+      wofDue: '',
+      rego: '',
+      regoDue: '',
     },
   ]
   const defaultConsumables = [
     {
       name: 'Engine Oil',
+      replaced: '',
       km: 10000,
+      due: '',
       user: user.id,
     },
     {
       name: 'Brake Oil',
+      replaced: '',
       km: 40000,
+      due: '',
       user: user.id,
     },
     {
       name: 'Mission Oil',
+      replaced: '',
       km: 80000,
+      due: '',
       user: user.id,
     },
     {
       name: 'Distilled Water',
+      replaced: '',
       km: 20000,
+      due: '',
       user: user.id,
     },
     {
       name: 'Battery',
+      replaced: '',
       km: 50000,
+      due: '',
       user: user.id,
     },
     {
       name: 'Tyre',
+      replaced: '',
       km: 40000,
+      due: '',
       user: user.id,
     },
   ]
