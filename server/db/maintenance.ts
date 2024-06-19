b50adb65cc5281de6f2bca901ce77c9109c345d3
Moa Ha <moahaa03@gmail.com>
2024-06-19 23:32:43 +1200

    return object

--- server/db/maintenance.ts
+++ server/db/maintenance.ts
@@ -2,7 +2,7 @@ import db from './connection'
 import { Wof } from '../../models/maintenance'
 
 export async function getMaintenance(id: string) {
-  return await db('maintenance').where('user', id)
+  return await db('maintenance').where('user', id).first()
 }
 
 export async function updateWof(id: string, data: Wof) {
