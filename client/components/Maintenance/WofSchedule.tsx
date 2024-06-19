aeff6fa499149b6a62db514c4e14b394afd257dc
Moa Ha <moahaa03@gmail.com>
2024-06-19 23:35:28 +1200

    separate the save button for mutation

--- client/components/Maintenance/WofSchedule.tsx
+++ client/components/Maintenance/WofSchedule.tsx
@@ -2,15 +2,22 @@ import React, { useState } from 'react'
 import CarYear from './CarYear'
 import { calculate } from '../function'
 import { Maintenance } from '../../../models/maintenance'
+import { useUpdateWof } from '../../hooks/useMaintenance'
 
 function WofSchedule({ data }: { data: Maintenance }) {
   const [date, setDate] = useState(data.wof)
   const [old, setOld] = useState(false)
+  const [wof, setWof] = useState({
+    user: data.user,
+    wof: data.wof,
+    wofDue: data.wofDue,
+  })
 
+  const mutation = useUpdateWof()
   function handleClick() {
     setOld(!old)
   }
-  let due
+  let due: string
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setDate(e.target.value)
@@ -23,6 +30,11 @@ function WofSchedule({ data }: { data: Maintenance }) {
       due = calculate(date, 365)
     }
     console.log(due)
+    setWof({ user: data.user, wof: date, wofDue: due })
+  }
+
+  function handleSave() {
+    mutation.mutate(wof)
   }
 
   return (
@@ -39,14 +51,17 @@ function WofSchedule({ data }: { data: Maintenance }) {
           type="date"
           name="date"
           id="datePicker"
-          value={date}
+          value={data.wof}
         />
-        <button className="btn-clear">save</button>
+        <button className="btn-clear">Check the next schedule</button>
         <p>
           Due:
-          <span className="returned-date"> {due}</span>❕
+          <span className="returned-date"> {wof.wofDue}</span>❕
         </p>
       </form>
+      <button className="btn-clear" onClick={handleSave}>
+        Save
+      </button>
     </div>
   )
 }
