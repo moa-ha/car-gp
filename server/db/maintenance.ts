1f75f74f705f19cb90dffc724d4250c3a73a4b3e
Moa Ha <moahaa03@gmail.com>
2024-06-21 18:32:23 +1200

    rego update BE

--- server/db/maintenance.ts
+++ server/db/maintenance.ts
@@ -1,10 +1,14 @@
 import db from './connection'
-import { Wof } from '../../models/maintenance'
+import { Rego, Wof } from '../../models/maintenance'
 
 export async function getMaintenance(id: string) {
   return await db('maintenance').where('user', id).first()
 }
 
 export async function updateWof(id: string, data: Wof) {
+  await db('maintenance').where('user', id).update(data)
+}
+
+export async function updateRego(id: string, data: Rego) {
   await db('maintenance').where('user', id).update(data)
 }
--- server/routes/maintenance.ts
+++ server/routes/maintenance.ts
@@ -3,6 +3,7 @@ import { Router } from 'express'
 import * as db from '../db/maintenance.ts'
 import { StatusCodes } from 'http-status-codes'
 import { JwtRequest } from '../auth0.ts'
+import { Jwt } from 'jsonwebtoken'
 
 const router = Router()
 
@@ -27,6 +28,18 @@ router.patch('/', async (req: JwtRequest, res) => {
   } catch (e) {
     console.log(e)
     res.status(500).json({ message: "Couldn't update wof due dates" })
+  }
+})
+
+router.patch('/', async (req: JwtRequest, res) => {
+  const id = String(req.auth?.sub)
+  const data = req.body
+  try {
+    await db.updateRego(id, data)
+    res.setHeader('Location', req.baseUrl).sendStatus(StatusCodes.CREATED)
+  } catch (e) {
+    console.log(e)
+    res.status(500).json({ message: 'Error in updating rego' })
   }
 })
 
