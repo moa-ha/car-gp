1758c7318f17d946e4b9b77f7e69353dd16b6b82
Moa Ha <moahaa03@gmail.com>
2024-07-07 18:14:04 +1200

    try connection change

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -6,19 +6,44 @@ const __dirname = Path.dirname(__filename)
 
 export default {
   development: {
-    client: 'pg',
-    useNullAsDefault: true,
-    connection: process.env.DATABASE_URL,
+    client: 'postgresql',
+    connection: {
+      host: process.env.POSTGRES_HOST,
+      database: process.env.POSTGRES_DATABASE,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
     migrations: {
       directory: './db/migrations',
     },
     seeds: {
       directory: './db/seeds',
     },
+  },
+  production: {
+    client: 'postgresql',
+    connection: {
+      connectionString: process.env.POSTGRES_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
     pool: {
       min: 2,
       max: 10,
-      // afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
+    },
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
     },
   },
 
@@ -35,26 +60,18 @@ export default {
       directory: Path.join(__dirname, 'seeds'),
     },
     pool: {
-      min: 2,
-      max: 10,
-      // afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
+      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
     },
   },
 
-  production: {
-    client: 'pg',
-    useNullAsDefault: true,
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: './db/migrations',
-    },
-    seeds: {
-      directory: './db/seeds',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-      // afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
-    },
-  },
+  // production: {
+  //   client: 'sqlite3',
+  //   useNullAsDefault: true,
+  //   connection: {
+  //     filename: '/app/storage/prod.sqlite3',
+  //   },
+  //   pool: {
+  //     afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
+  //   },
+  // },
 }
