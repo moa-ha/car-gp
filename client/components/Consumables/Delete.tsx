47049d07b7d8c4a833639d20f74ad7d6c917aa52
Moa Ha <moahaa03@gmail.com>
2024-08-01 15:56:13 +1200

    confirm before delete

--- client/components/Consumables/Delete.tsx
+++ client/components/Consumables/Delete.tsx
@@ -3,7 +3,9 @@ import { useDeleteConsumable } from '../../hooks/useConsumables'
 function Delete({ id }: { id: number }) {
   const mutation = useDeleteConsumable()
   const handleDelete = () => {
-    mutation.mutate(id)
+    if (confirm('Do you want to delete this item?')) {
+      mutation.mutate(id)
+    }
   }
 
   return (
