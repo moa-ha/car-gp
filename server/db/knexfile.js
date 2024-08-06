5c5d275aa3c299ee0938c61942854423ead6db37
Moa Ha <moahaa03@gmail.com>
2024-08-06 22:24:35 +1200

    update knex

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,5 +1,12 @@
 import * as Path from 'node:path'
 import * as URL from 'node:url'
+import dotenv from 'dotenv'
+import path from 'path'
+// import { log } from 'node:console'
+
+dotenv.config()
+
+const databasePath = process.env.TURSO_DATABASE_URL || ''
 
 const __filename = URL.fileURLToPath(import.meta.url)
 const __dirname = Path.dirname(__filename)
@@ -10,6 +17,12 @@ export default {
     useNullAsDefault: true,
     connection: {
       filename: Path.join(__dirname, 'dev.sqlite3'),
+    },
+    migrations: {
+      directory: Path.join(__dirname, 'migrations'),
+    },
+    seeds: {
+      directory: Path.join(__dirname, 'seeds'),
     },
     pool: {
       afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
@@ -37,7 +50,14 @@ export default {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: '/app/storage/prod.sqlite3',
+      filename: databasePath,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: Path.join(__dirname, 'migrations'),
+    },
+    seeds: {
+      directory: Path.join(__dirname, 'seeds'),
     },
     pool: {
       afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
