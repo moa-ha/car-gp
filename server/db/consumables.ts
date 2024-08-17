60f692be14fdfaf37ed858cc5c0d1d7bae66990f
Moa Ha <moahaa03@gmail.com>
2024-08-17 14:31:52 +1200

    change db connection from knex to drizzle

--- server/db/consumables.ts
+++ server/db/consumables.ts
@@ -1,22 +1,49 @@
-import db from './connection.ts'
-import { Consumable, ConsumableData } from '../../models/consumable.ts'
+// import db from './connection.ts'
+// import { Consumable, ConsumableData } from '../../models/consumable.ts'
 
-export async function getConsumablesByUser(id: string): Promise<Consumable[]> {
-  return db('consumables').where('user', id)
+// export async function getConsumablesByUser(id: string): Promise<Consumable[]> {
+//   return db('consumables').where('user', id)
+// }
+
+// export async function getConsumableById(id: number) {
+//   return await db('consumables').where({ id }).first()
+// }
+
+// export async function addConsumable(data: ConsumableData) {
+//   await db('consumables').insert(data)
+// }
+
+// export async function deleteConsumable(id: number) {
+//   await db('consumables').where({ id }).del()
+// }
+
+// export async function editConsumable(id: number, data: ConsumableData) {
+//   await db('consumables').where({ id }).update(data)
+// }
+
+import { eq } from 'drizzle-orm'
+import { db } from '../../src/db/index'
+import { consumables } from '../../src/db/schema'
+import { ConsumableData } from '../../models/consumable'
+
+export async function getConsumablesByUser(id: string) {
+  console.log('using turso db')
+
+  return db.select().from(consumables).where(eq(consumables.user, id))
 }
 
 export async function getConsumableById(id: number) {
-  return await db('consumables').where({ id }).first()
+  return db.select().from(consumables).where(eq(consumables.id, id))
 }
 
-export async function addConsumable(data: ConsumableData) {
-  await db('consumables').insert(data)
+export async function addConsumable(id: string, data: ConsumableData) {
+  await db.insert(consumables).values({ ...data, user: id })
 }
 
 export async function deleteConsumable(id: number) {
-  await db('consumables').where({ id }).del()
+  await db.delete(consumables).where(eq(consumables.id, id))
 }
 
 export async function editConsumable(id: number, data: ConsumableData) {
-  await db('consumables').where({ id }).update(data)
+  await db.update(consumables).set(data).where(eq(consumables.id, id))
 }
--- server/db/maintenance.ts
+++ server/db/maintenance.ts
@@ -1,14 +1,31 @@
-import db from './connection'
+// import db from './connection'
+// import { Rego, Wof } from '../../models/maintenance'
+
+// export async function getMaintenance(id: string) {
+//   return await db('maintenance').where('user', id).first()
+// }
+
+// export async function updateWof(id: string, data: Wof) {
+//   await db('maintenance').where('user', id).update(data)
+// }
+
+// export async function updateRego(id: string, data: Rego) {
+//   await db('maintenance').where('user', id).update(data)
+// }
+
+import { eq } from 'drizzle-orm'
+import { db } from '../../src/db/index'
+import { maintenance } from '../../src/db/schema'
 import { Rego, Wof } from '../../models/maintenance'
 
 export async function getMaintenance(id: string) {
-  return await db('maintenance').where('user', id).first()
+  return db.select().from(maintenance).where(eq(maintenance.user, id))
 }
 
 export async function updateWof(id: string, data: Wof) {
-  await db('maintenance').where('user', id).update(data)
+  await db.update(maintenance).set(data).where(eq(maintenance.user, id))
 }
 
 export async function updateRego(id: string, data: Rego) {
-  await db('maintenance').where('user', id).update(data)
+  await db.update(maintenance).set(data).where(eq(maintenance.user, id))
 }
