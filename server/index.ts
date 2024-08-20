c2a8bd0238a1e795fa1f0dd5d843875820a01098
Moa Ha <moahaa03@gmail.com>
2024-08-20 16:25:04 +1200

    config

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import server from './server.ts'
 
 const PORT = process.env.PORT || 3000
 
-server.listen(PORT, () => {
+server.listen(Number(PORT), '0.0.0.0', () => {
   // eslint-disable-next-line no-console
   console.log('Server listening on port', PORT)
 })
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
+      '/api': 'http://0.0.0.0:3000',
     },
   },
 })
