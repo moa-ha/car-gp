a75f5d812ca5b54b2b65890607254ce5321fd7e0
Moa Ha <moahaa03@gmail.com>
2024-08-21 15:59:00 +1200

    load sign in page

--- client/pages/Consumables/Consumables.tsx
+++ client/pages/Consumables/Consumables.tsx
@@ -2,35 +2,39 @@ import { Link } from 'react-router-dom'
 import { useConsumables } from '../../hooks/useConsumables'
 import Delete from '../../components/Consumables/Delete'
 import DateInput from '../../components/Consumables/DateInput'
+import SignIn from '../SignIn'
 
 function Consumables() {
   const { data } = useConsumables()
-
-  return (
-    <div className="p-4">
-      <div>
-        <Link to="/consumables/add">
-          <button className="btn-clear">Add your item</button>
-        </Link>
+  if (data) {
+    return (
+      <div className="p-4">
+        <div>
+          <Link to="/consumables/add">
+            <button className="btn-clear">Add your item</button>
+          </Link>
+        </div>
+        <div>
+          <ul className="flex w-full flex-wrap gap-4">
+            {data &&
+              data.map((consumable) => (
+                <li key={consumable.id} className="bg-opacity w-80 p-4">
+                  <Link to={`/consumables/${consumable.id}`}>
+                    <button className="btn-clear relative">Edit</button>
+                  </Link>
+                  <Delete id={consumable.id} />
+                  <br></br>
+                  <span className="relative text-2xl">{consumable.name}</span>
+                  <DateInput id={consumable.id} />
+                </li>
+              ))}
+          </ul>
+        </div>
       </div>
-      <div>
-        <ul className="flex w-full flex-wrap gap-4">
-          {data &&
-            data.map((consumable) => (
-              <li key={consumable.id} className="bg-opacity w-80 p-4">
-                <Link to={`/consumables/${consumable.id}`}>
-                  <button className="btn-clear relative">Edit</button>
-                </Link>
-                <Delete id={consumable.id} />
-                <br></br>
-                <span className="relative text-2xl">{consumable.name}</span>
-                <DateInput id={consumable.id} />
-              </li>
-            ))}
-        </ul>
-      </div>
-    </div>
-  )
+    )
+  } else {
+    return <SignIn />
+  }
 }
 
 export default Consumables
