0242263ee5386e0d3e44c182cce4b8f6d625bded
Moa Ha <moahaa03@gmail.com>
2024-08-21 15:03:05 +1200

    user icon

--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -4,7 +4,7 @@ import maintenance from '../styles/images/maintenance.png'
 import consumables from '../styles/images/consumables.png'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faUser, faSmile } from '@fortawesome/free-solid-svg-icons'
 
 import {
   IfAuthenticated,
@@ -42,6 +42,7 @@ function Home() {
   return (
     <div className="h-full p-5">
       <div>
+        <FontAwesomeIcon icon={faUser} />
         <IfAuthenticated>
           <button className="btn-clear" onClick={handleSignOut}>
             Sign out
@@ -59,10 +60,12 @@ function Home() {
           </button>
         </IfNotAuthenticated>
       </div>
+
       <div className="mb-20 flex flex-col items-center">
         <img src={logo} alt="home-logo" className="w-64" />
         <img src={name} alt="home-logo-text" className="w-80" />
       </div>
+
       <div className="grid justify-items-center">
         <Link to="/guest">
           <div
@@ -70,7 +73,7 @@ function Home() {
             className="bg-opacity mb-4 w-96 p-4 text-center"
           >
             <span className="relative w-64 text-2xl">
-              <FontAwesomeIcon icon={faUser} /> Try as a Guest without Login
+              <FontAwesomeIcon icon={faSmile} /> Try as a Guest without Login
             </span>
           </div>
         </Link>
@@ -78,7 +81,8 @@ function Home() {
         <ul className="mb-4 flex gap-4">
           <Link to="/maintenance">
             <li key="Maintenance" className="bg-opacity p-4">
-              <span className="relative text-2xl">Maintenance</span>
+              <span className="relative text-2xl">Maintenance </span>
+              <span className="text-sm italic">Login is required</span>
               <img
                 src={maintenance}
                 alt="maintenance"
@@ -86,9 +90,11 @@ function Home() {
               />
             </li>
           </Link>
+
           <Link to="/consumables">
             <li key="Consumables" className="bg-opacity p-4">
-              <span className="relative text-2xl">Consumables</span>
+              <span className="relative text-2xl">Consumables </span>
+              <span className="text-sm italic">Login is required</span>
               <img
                 src={consumables}
                 alt="consumables"
